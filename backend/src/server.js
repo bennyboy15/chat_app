b68e21186e1c81c4277355bd9343defe22a63480
Benjamin Harvey <benjaminharvey2000@gmail.com>
2025-09-28 21:09:18 +0930

    Setup project and connection to MongoDB

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, (req,res) => {
+    console.log(`Server running @ http://localhost:${PORT}`);
+    connectDB();
+});
