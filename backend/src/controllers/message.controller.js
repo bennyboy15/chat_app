9fa705f8a11dca86d062ff231f5cdae1a80cfbce
BenjaminHarveyCMVTC <benjamin.harvey@cmvtruckcentre.com.au>
2025-10-02 10:29:43 +0930

    Message model & API

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,60 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export async function getUsersForSidebar(req,res) {
+    try {
+        const currentUserId = req.user._id;
+        const users = await User.find({_id: {$ne: currentUserId}}).select("-password");
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export async function getMessages(req,res) {
+    try {
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                {senderId: senderId, receiverId:receiverId},
+                {senderId: receiverId, receiverId:senderId},
+            ]
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+        console.error("Error in getMessages: ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export async function sendMessage(req,res) {
+    try {
+        const {text,image} = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+        
+        let imageURL;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageURL = uploadResponse.secure_url;
+        };
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageURL,
+        });
+
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.error("Error in sendMessage: ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
