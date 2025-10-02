8925af8bd351faadafd9b418b52e674622a0270e
BenjaminHarveyCMVTC <benjamin.harvey@cmvtruckcentre.com.au>
2025-10-02 11:10:19 +0930

    Frontend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,14 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: "http://localhost:5173",credentials: true}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
