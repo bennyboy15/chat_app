d35b8a83425a6d9ba862e5498c209bb1a5aa2a18
Benjamin Harvey <benjaminharvey2000@gmail.com>
2025-10-04 19:52:52 +0930

    Deployment commands

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,28 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {app,server} from "./lib/socket.js";
+import path from "path";
 
 //const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: "http://localhost:5173",credentials: true}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, (req,res) => {
     console.log(`Server running @ http://localhost:${PORT}`);
